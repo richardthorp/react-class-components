f3a7bde369c8951dd3b9d419a1530f226b1813b6
Rich Thorp <rich@whitespace.studio>
2025-03-30 13:32:58 +0200

    Change Users to class component

--- src/classComponents/Users.js
+++ src/classComponents/Users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Component } from 'react';
 import User from './User';
 
 import classes from '../components/Users.module.css';
@@ -9,29 +9,39 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showUsers: true
+    };
+  }
 
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
+  toggleUsersHandler() {
+    // React merges state when using the setState method. If there was another property in the
+    // this.state object, we don't need to worry about it being removed in the method below
+    this.setState((currentState => {
+      return { showUsers: !currentState.showUsers }
+    }));
   };
 
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
-
-  return (
-    <div className={classes.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
-      </button>
-      {showUsers && usersList}
-    </div>
-  );
-};
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
 
 export default Users;
